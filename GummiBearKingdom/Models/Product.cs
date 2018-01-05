a27b16a59c02fdd24ef7fae1775c6dd9d01c762d
Kimberly Bordon <kbordon@gmail.com>
2018-01-05 09:52:42 -0800

    * GummiBearKingdom.csproj: add packages * Product.cs: create product class

---
+++ GummiBearKingdom/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GummiBearKingdom.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
