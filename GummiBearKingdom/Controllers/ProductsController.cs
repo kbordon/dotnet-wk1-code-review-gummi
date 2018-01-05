bc519cfca679d5495b8f0b9a8f280fc386263373
Kimberly Bordon <kbordon@gmail.com>
2018-01-05 11:26:41 -0800

    * HomeController.cs: Add controller for home
    * ProductsController.cs: Add contoller for products
    
    * Product.cs:
    * GummiBearKingdom.csproj:
    
    * Index.cshtml: add home controller index
    
    * Index.cshtml: add product controller index
    
    * _Layout.cshtml: add shared layout

---
+++ GummiBearKingdom/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GummiBearKingdom.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GummiBearKingdom.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private GummiBearKingdomDbContext db = new GummiBearKingdomDbContext();
+        public IActionResult Index()
+        {
+            List<Product> model = db.Products.ToList();
+            return View();
+        }
+    }
+}
--- GummiBearKingdom/Models/Product.cs
+++ GummiBearKingdom/Models/Product.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GummiBearKingdom.Models
 {
+    [Table("products")]
     public class Product
     {
         [Key]
