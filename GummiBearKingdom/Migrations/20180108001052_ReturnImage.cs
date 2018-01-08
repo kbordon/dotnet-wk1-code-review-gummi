a80a6b79275f0c87e281249f717cb1cf7b179e4c
Kimberly Bordon <kbordon@gmail.com>
2018-01-07 23:07:26 -0800

    add new migration

---
+++ GummiBearKingdom/Migrations/20180108001052_ReturnImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GummiBearKingdom.Migrations
+{
+    public partial class ReturnImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "products");
+        }
+    }
+}
