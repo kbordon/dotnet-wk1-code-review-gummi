e3f252742144d9b86d5bc55461ef30ae0b1eff85
Kimberly Bordon <kbordon@gmail.com>
2018-01-12 08:23:06 -0800

    Will override equals method, and start Review model.

--- GummiBearKingdom/Models/Product.cs
+++ GummiBearKingdom/Models/Product.cs
@@ -14,5 +14,24 @@ namespace GummiBearKingdom.Models
         public string Description { get; set; }
         public decimal Cost { get; set; }
         public byte[] Image { get; set; }
+
+        public override bool Equals(System.Object otherProduct)
+        {
+            if (!(otherProduct is Product))
+            {
+                return false;
+            }
+            else
+            {
+                Product newProduct = (Product)otherProduct;
+                return this.ProductId.Equals(newProduct.ProductId);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ProductId.GetHashCode();
+        }
     }
+
 }
---
+++ GummiBearKingdom/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("reviews")]
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public string Author { get; set; }
+        public string ContentBody { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
