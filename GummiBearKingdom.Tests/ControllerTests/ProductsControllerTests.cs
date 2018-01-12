da0a9e3a2605275f172e1082b07687849f946e1d
Kimberly Bordon <kbordon@gmail.com>
2018-01-12 12:31:40 -0800

    Start test for ProductsController.

---
+++ GummiBearKingdom.Tests/ControllerTests/ProductsControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GummiBearKingdom.Controllers;
+using System.IO;
+using System;
+
+namespace GummiBearKingdom.Tests
+{
+    [TestClass]
+    public class ProductsControllerTests
+    {
+        [TestMethod]
+        public void GetName_ReturnsProductName_String()
+        {
+        }
+
+    }
+}
