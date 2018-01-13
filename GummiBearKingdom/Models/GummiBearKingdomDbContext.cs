9e14798100b5c7176328e64eca54d77d446d40ef
Kimberly Bordon <kbordon@gmail.com>
2018-01-12 16:05:46 -0800

    Add test database with migrations.

--- GummiBearKingdom/Models/GummiBearKingdomDbContext.cs
+++ GummiBearKingdom/Models/GummiBearKingdomDbContext.cs
@@ -4,24 +4,25 @@ namespace GummiBearKingdom.Models
 {
     public class GummiBearKingdomDbContext : DbContext
     {
-        public GummiBearKingdomDbContext()
-        {
-        }
 
-        public DbSet<Product> Products { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseMySql(@"Server=localhost;Port=8889;database=gummibearkingdom;uid=root;pwd=root;");
         }
 
-        public GummiBearKingdomDbContext(DbContextOptions<GummiBearKingdomDbContext> options) : base(options)
-        {
-        }
-
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
         }
+
+		public GummiBearKingdomDbContext()
+		{
+		}
+        public GummiBearKingdomDbContext(DbContextOptions<GummiBearKingdomDbContext> options) : base(options)
+        {
+        }
+
     }
 }
---
+++ GummiBearKingdom/Models/TestDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+               
+namespace GummiBearKingdom.Models
+{
+    public class TestDbContext : GummiBearKingdomDbContext
+    {
+        public override DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=gummibearkingdom_test;uid=root;pwd=root;");
+        }
+    }
+}
