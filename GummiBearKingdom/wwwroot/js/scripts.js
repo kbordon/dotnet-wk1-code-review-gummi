1c12a1afd5d39ef492f594afb63bcceb5b385c71
Kimberly Bordon <kbordon@gmail.com>
2018-01-14 18:57:11 -0800

    Edit review form style, and add modal hide and show functionality.

---
+++ GummiBearKingdom/wwwroot/js/scripts.js
@@ -0,0 +1,8 @@
+﻿function toggleModal() {
+    var x = document.getElementById("modal-review");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+} 
